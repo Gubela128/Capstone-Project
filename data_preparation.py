058b8d4ef4d8c556b63717bf61feb683c9cb3de3
Gubela128 <gubeladze.03@gmail.com>
2024-04-19 21:35:42 +0400

    adding lemmatizer

--- data_preparation.py
+++ data_preparation.py
@@ -1,12 +1,11 @@
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-
+from nltk.stem import WordNetLemmatizer
 
 class DataPreparation:
     def __init__(self):
         nltk.download('stopwords', quiet=True)
+        nltk.download('wordnet', quiet=True)
         self.stop_words = set(stopwords.words('english'))
+        self.lemmatizer = WordNetLemmatizer()
 
     def lower_case(self, data):
         data['text'] = data['text'].str.lower()
@@ -21,4 +20,11 @@ class DataPreparation:
         data['text'] = data['text'].apply(remove_stopwords_from_text)
         return data
 
+    def lemmatize_text(self, data):
+        def lemmatize_text_string(text):
+            tokens = word_tokenize(text)
+            lemmatized_tokens = [self.lemmatizer.lemmatize(word) for word in tokens]
+            return ' '.join(lemmatized_tokens)
 
+        data['text'] = data['text'].apply(lemmatize_text_string)
+        return data
