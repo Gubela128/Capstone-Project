7406cc238a46408124502094c7d18c34bc245f63
natia02 <pruidze.natia@kiu.edu.ge>
2024-04-24 22:14:24 +0400

    add in data text in lowe case method

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,23 +1,22 @@
+import json
+
 from data_preparation import DataPreparation
-import pandas as pd
 
 
 class EmotionDetection:
 
     @classmethod
     def read_data(cls, file_name):
-        data = pd.read_csv(f"data/{file_name}")
-        text = data["text"]
-        return text
+        with open(file_name, 'r') as file:
+            data = json.load(file)
+        return data
 
     @classmethod
     def save_prepared_data(cls):
-        training_data = cls.read_data('training.csv')
+        training_data = cls.read_data('data/training.json')
         data_preparation = DataPreparation()
         training_data = data_preparation.lower_case(training_data)
-        training_data = data_preparation.stop_words(training_data)
-        training_data = data_preparation.lemmatizer(training_data)
-        training_data.to_csv(f'data/processed_training_data.csv', index=False)
+        print(training_data[0])
 
     @classmethod
     def main(cls):
