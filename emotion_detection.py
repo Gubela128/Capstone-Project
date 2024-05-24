e1c561aa19475ac53fd2fae237e2f38f3d257513
natia02 <pruidze.natia@kiu.edu.ge>
2024-05-24 13:30:48 +0400

    remove sremer and post tag functions

--- emotion_detection.py
+++ emotion_detection.py
@@ -20,8 +20,7 @@ class EmotionDetection:
         training_data = data_preparation.remove_special_characters(training_data)
         training_data = data_preparation.lemmatize_text(training_data)
         training_data = data_preparation.handle_negations(training_data)
-        training_data = data_preparation.stem_text(training_data)
-        training_data = data_preparation.pos_tagging(training_data)
+
         with open('data/prepared_training_data.json', 'w') as file:
             json.dump(training_data, file, indent=4)
 
